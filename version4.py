7c1654aa9e68af9e354a210612cc8a03349d3101
Jkiino <115900638+MyMeleGine@users.noreply.github.com>
2024-11-22 08:43:08 -0500

    Create version4.py

---
+++ version4.py
@@ -0,0 +1,35 @@
+import random
+
+def get_user_choice():
+    choice = input("Choose Rock, Paper, or Scissors: ")
+    return choice
+
+def get_computer_choice():
+    choices = ["Rock", "Paper", "Scissors"]
+    choice = random.choice(choices)
+    return choice
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    if user_choice == "Rock" and computer_choice == "Scissors":
+        return "User wins!"
+    if user_choice == "Paper" and computer_choice == "Rock":
+        return "User Wins!"
+    if user_choice == "Scissors" and computer_choice == "Paper":
+        return "User has won!"
+    
+    return "Computer wins!"
+
+def play_game():
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    round_result = determine_winner(user_choice, computer_choice)
+    
+    print(f"User chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+    print(f"Result: {round_result}") 
+
+play_game()
+    
